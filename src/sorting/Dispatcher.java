914070003cce7f9c2e3c1df28e25e59b0f218f16
Maks Burenko <Cherniymaks@yandex.ru>
2022-01-10 14:56:53 +0300

    work programm

--- src/sorting/Dispatcher.java
+++ src/sorting/Dispatcher.java
@@ -2,27 +2,57 @@ package sorting;
 import java.util.Arrays;
 public class Dispatcher {
     public static void main(String[] args) {
-        Car c1 = new Car (200, 7000, "BMW", "Red");
-        Car c2 = new Car (120, 1000, "Mers", "Green");
-        Car c3 = new Car (190, 9000, "Honda", "White");
+        Car c1 = new Car(200, 7000, "BMW", "Red");
+        Car c2 = new Car(120, 1000, "Mers", "Green");
+        Car c3 = new Car(190, 9000, "Honda", "White");
 
-        Car [] c = {c1, c2, c3};
+        Car[] c = {c1, c2, c3};
 
-        Arrays.sort(c);
-        for (Car temp : c) {
-            System.out.println(temp);
-        }
-        Arrays.sort(c, new ComparatorByColor());
-        for (Car temp : c) {
-            System.out.println(temp);
-        }
-        Arrays.sort(c, new ComparatorByPrice());
-        for (Car temp : c) {
-            System.out.println(temp);
-        }
-        Arrays.sort(c, new ComparatorBySpeed());
-        for (Car temp : c) {
-            System.out.println(temp);
+        if (args.length == 0) {
+            Arrays.sort(c);
+            for (Car temp : c) {
+                System.out.println(temp);
+            }
+        } else {
+            if(int enterValue = Integer.parseInt(args[0]);) {
+
+            } else {
+
+            }
+
+
+            if (enterValue == 1) {
+                Arrays.sort(c);
+                for (Car temp : c) {
+                    System.out.println(temp);
+                }
+            } else {
+                if (enterValue == 2) {
+                    Arrays.sort(c, new ComparatorByColor());
+                    for (Car temp : c) {
+                        System.out.println(temp);
+                    }
+                } else {
+                    if (enterValue == 3) {
+                        Arrays.sort(c, new ComparatorByPrice());
+                        for (Car temp : c) {
+                            System.out.println(temp);
+                        }
+                    } else {
+                        if (enterValue == 4) {
+                            Arrays.sort(c, new ComparatorBySpeed());
+                            for (Car temp : c) {
+                                System.out.println(temp);
+                            }
+                        } else {
+                            Arrays.sort(c);
+                            for (Car temp : c) {
+                                System.out.println(temp);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
